d9809e6b5e68ac1376fedb22303c3e96d985a6f9
ihor melnyk <ih.melnyk@gmail.com>
2021-02-24 11:18:59 +0200

    One more example.

--- djangoProject/invoices/models.py
+++ djangoProject/invoices/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
+from djangoProject.payments.models import Payment
+
 
 class Invoice(models.Model):
     amount = models.IntegerField()
-    payment = models.ForeignKey(to='payments.Payment', on_delete=models.CASCADE)
+    payment = models.ForeignKey(to=Payment, on_delete=models.CASCADE)
--- djangoProject/payments/views.py
+++ djangoProject/payments/views.py
@@ -1,3 +1,9 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 
-# Create your views here.
+from djangoProject.payments.models import find_bigger_payment_for_invoice
+
+
+def my_view(request):
+    response = find_bigger_payment_for_invoice()
+    return JsonResponse({'response': 'Hello World'})
